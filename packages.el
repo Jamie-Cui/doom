0910ddf67070bd4a8a51ecf89d23c907a2f585b7
Jamie-Cui <jamie.cui@outlook.com>
2023-11-16 14:47:41 +0800

    Update packages.el

--- packages.el
+++ packages.el
@@ -9,7 +9,6 @@
 (package! flycheck-google-cpplint)
 (package! bazel)
 (package! latex-preview-pane) ;; latex
-(package! zotero)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
