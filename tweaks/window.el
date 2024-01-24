9f2b538d96ae627671f28b45eaafa328962e49cb
Jamie-Cui <jamie.cui@outlook.com>
2024-01-24 12:51:44 +0800

    Update window.el

--- tweaks/window.el
+++ tweaks/window.el
@@ -7,13 +7,13 @@
       ;; move by logical lines rather than visual lines (better for macros)
       line-move-visual nil)
 
-(add-to-list 'initial-frame-alist '(fullscreen . maximized)) ;; Maximized screen on doom start
-(add-to-list 'initial-frame-alist '(undecorated . t)) ;; no title bar
-(add-to-list 'initial-frame-alist '(tool-bar-lines . 0))
-(add-to-list 'initial-frame-alist '(menu-bar-lines . 0))
-(add-to-list 'initial-frame-alist '(vertical-scroll-bars . nil))
-(add-to-list 'initial-frame-alist '(horizontal-scroll-bars . nil))
+(add-to-list 'default-frame-alist '(fullscreen . maximized)) ;; Maximized screen on doom start
+(add-to-list 'default-frame-alist '(undecorated . t)) ;; no title bar
+(add-to-list 'default-frame-alist '(tool-bar-lines . 0))
+(add-to-list 'default-frame-alist '(menu-bar-lines . 0))
+(add-to-list 'default-frame-alist '(vertical-scroll-bars . nil))
+(add-to-list 'default-frame-alist '(horizontal-scroll-bars . nil))
 
 (map! :leader
       :desc "Switch to next frame" ;; Switch between different emacs frames
-      "W" #'+evil/next-frame)
+      "F" #'+evil/next-frame)
