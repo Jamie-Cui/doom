ccecba783000c7fc47a1a4064325ad3be3f17f84
Jamie-Cui <jamie.cui@outlook.com>
2024-02-07 13:37:46 +0800

    Update org.el

--- tweaks/org.el
+++ tweaks/org.el
@@ -17,14 +17,19 @@
     )
   )
 
+;; ----------------------------------------------------------------------------
+;; Configuration: note taking
+;; ----------------------------------------------------------------------------
+
 ;; Re-configure deft-mode keybindings
 (after! deft
   ;; start with evil normal mode
   (set-evil-initial-state! 'deft-mode 'normal))
 
 (after! citar
-  (add-to-list 'citar-notes-paths (concat org-remote-path "papers")) ; always use remote path
-  (add-to-list 'citar-bibliography (concat org-remote-path "zotero_all.bib"))) ; always use remote path
+  (add-to-list 'citar-notes-paths (concat org-remote-path "roam")) ; put paper notes in roam folder
+  (add-to-list 'citar-bibliography (concat org-remote-path "zotero_all.bib")); always use remote path
+  )
 
 ;; Setup org-latex-preview, load cryptocode, and scale the generated math imgs
 (after! org
@@ -36,6 +41,15 @@
   (setq org-return-follows-link t) ;; return in org now follows link (globally)
   (require 'org-download) ;; drag-and-drop for images
   )
+
+;; configure org-roam-uo
+(after! org-roam
+  (add-to-list 'load-path (concat doom-local-dir "straight/repos/org-roam-ui")) ;; manually load package
+  (add-to-list 'load-path (concat doom-local-dir "straight/repos/emacs-web-server")) ;; manually load package
+  (setq org-roam-ui-sync-theme t
+        org-roam-ui-follow t
+        org-roam-ui-update-on-save t
+        org-roam-ui-open-on-start t))
 
 ;; Setup org-download directory
 (after! org-download
@@ -53,17 +67,16 @@
   (keymap-set org-agenda-mode-map "RET" 'org-agenda-show-and-scroll-up)
   (keymap-set org-agenda-mode-map "SPC" nil))
 
-;; configure org-roam-uo
-(after! org-roam
-  (add-to-list 'load-path (concat doom-local-dir "straight/repos/org-roam-ui")) ;; manually load package
-  (add-to-list 'load-path (concat doom-local-dir "straight/repos/emacs-web-server")) ;; manually load package
-  (setq org-roam-ui-sync-theme t
-        org-roam-ui-follow t
-        org-roam-ui-update-on-save t
-        org-roam-ui-open-on-start t))
 
 ;; ----------------------------------------------------------------------------
 ;; My Package [latex-preview-pane]
 ;; ----------------------------------------------------------------------------
 (require 'latex-preview-pane)
 
+;; ----------------------------------------------------------------------------
+;; Config: openwith, see: https://emacs.stackexchange.com/questions/3105/how-to-use-an-external-program-as-the-default-way-to-open-pdfs-from-emacs
+;; ----------------------------------------------------------------------------
+;; (require 'openwith)
+;; (openwith-mode t)
+;; (setq openwith-confirm-invocation t)
+;; (setq openwith-associations '(("\\.pdf\\'" "preview" (file))))
