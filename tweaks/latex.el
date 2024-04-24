81934f0c57f403fc63586acf5211034791f3fe8f
Jamie-Cui <jamie.cui@outlook.com>
2024-04-24 21:18:42 +0800

    Update latex.el

--- tweaks/latex.el
+++ tweaks/latex.el
@@ -5,26 +5,40 @@
 (setq +latex-viewers '(skim))
 
 (after! tex
-  (map! :localleader
-        :map (LaTeX-mode-map)
-        :desc "Run TeX-master-file-ask" ;; ask for the master file of latex project
-        "t" #'TeX-master-file-ask)
-
-  ;; use remote bib for default
-  (setq reftex-default-bibliography (concat org-remote-path "zotero_all.bib"))
+  ;; use remote bib
+  ;; (setq reftex-default-bibliography (concat org-remote-path "zotero_all.bib"))
 
   ;; do not format with apheleia
   (setq-hook! 'LaTeX-mode-hook
     apheleia-inhibit t
     +format-with nil) ;; do not format with apheleia
 
+  (map! :after latex
+        :localleader
+        :map LaTeX-mode-map
+        :desc "Run Tex-Clean"             "c" #'TeX-clean
+        :desc "Run TeX-master-file-ask"   "m" #'TeX-master-file-ask
+        :desc "Set tex engine"            "e" #'TeX-engine-set)
 
   ;; Set to `auto' for continuation lines to be offset by `LaTeX-indent-line':
   ;;   \\item lines aligned
   ;;     like this, assuming `LaTeX-indent-line' == 2
   (setq +latex-indent-item-continuation-offset 'auto)
+
+  ;; set default tex-engine to xetex
+  (setq TeX-engine 'xetex)
   )
 
+;; (after! tex-mode
+;;   (set-company-backend! 'company-yasnippet))
+
+;; enable yasnippet in tex-mode
+(add-hook! 'tex-mode
+  (lambda ()
+    (set (make-local-variable 'company-backends)
+         '((company-dabbrev-code company-yasnippet)))))
 
 ;; enable evil on pdf-view-mode
 (evil-set-initial-state 'pdf-view-mode 'normal)
+
+
