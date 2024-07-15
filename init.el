80630f7da31fb20502693abdf65dd2015e2b3aa7
Jamie-Cui <jamie.cui@outlook.com>
2024-07-15 13:48:29 +0800

    Update init.el

--- init.el
+++ init.el
@@ -59,7 +59,7 @@
        (format +onsave)              ; automated prettiness
        ;;god               ; run Emacs commands without modifier keys
        ;;lispy             ; vim for lisp, for people who don't like vim
-       ;;multiple-cursors  ; editing in many places at once
+       multiple-cursors  ; editing in many places at once
        ;;objed             ; text object editing for the innocent
        ;;parinfer          ; turn lisp into python, sort of
        ;;rotate-text       ; cycle region at point between text candidates
@@ -176,7 +176,7 @@
        ;;zig               ; C, but simpler
 
        :email
-       ;;(mu4e +org +gmail)
+       ;; (mu4e +org)
        ;;notmuch
        ;;(wanderlust +gmail)
 
