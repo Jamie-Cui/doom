268e038eb1a83b70fe1182cccea27fb49bacb73d
Jamie Cui <jamie.cui@outlook.com>
2024-09-13 23:13:48 +0800

    tweak: use remote path && disable file-templates

--- config.el
+++ config.el
@@ -25,7 +25,7 @@
 ;; setup new const variables, make sure to doom sync after change
 (defconst jamie-org-remote-path "~/Library/Mobile Documents/com~apple~CloudDocs/org-remote/")
 (defconst jamie-org-local-path "~/org-local/")
-(defconst jamie-use-remote-path nil) ; non-nil to use remote path
+(defconst jamie-use-remote-path 't) ; non-nil to use remote path
 
 ;; Don't ask, just quit
 (setq confirm-kill-emacs nil)
