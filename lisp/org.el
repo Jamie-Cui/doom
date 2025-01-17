69bc9c99d0d9498878fa2be331c5e59bc12223de
Jamie <jamie.cui@outlook.com>
2025-01-17 11:37:57 +0800

    Update org.el

--- lisp/org.el
+++ lisp/org.el
@@ -140,7 +140,10 @@
 
 (setq! org-agenda-files (list
                          (expand-file-name
-                          (concat (+org/get-org-directory) "/todo.org"))))
+                          (concat (+org/get-org-directory) "/todo.org"))
+                         (expand-file-name
+                          (concat (+org/get-org-directory) "/todo.org_archive"))))
+
 
 (after! org
   (setq org-startup-folded 'content)
@@ -181,6 +184,32 @@
                   ((org-agenda-overriding-header "Review Finished Task: This Year")))
             ))
           ))
+  (setq! org-capture-templates
+         '(("t" "Personal todo" entry
+            (file+headline +org-capture-todo-file "Inbox")
+            "* [ ] %?\n%i\n%a" :prepend t)
+           ;; ("n" "Personal notes" entry
+           ;;  (file+headline +org-capture-notes-file "Inbox")
+           ;;  "* %u %?\n%i\n%a" :prepend t)
+           ;; ("j" "Journal" entry
+           ;;  (file+olp+datetree +org-capture-journal-file)
+           ;;  "* %U %?\n%i\n%a" :prepend t)
+           ;; ("p" "Templates for projects")
+           ;; ("pt" "Project-local todo" entry
+           ;;  (file+headline +org-capture-project-todo-file "Inbox")
+           ;;  "* TODO %?\n%i\n%a" :prepend t)
+           ;; ("pn" "Project-local notes" entry
+           ;;  (file+headline +org-capture-project-notes-file "Inbox")
+           ;;  "* %U %?\n%i\n%a" :prepend t)
+           ;; ("pc" "Project-local changelog" entry
+           ;;  (file+headline +org-capture-project-changelog-file "Unreleased")
+           ;;  "* %U %?\n%i\n%a" :prepend t)
+           ;; ("o" "Centralized templates for projects")
+           ;; ("ot" "Project todo" entry #'+org-capture-central-project-todo-file "* TODO %?\n %i\n %a" :heading "Tasks" :prepend nil)
+           ;; ("on" "Project notes" entry #'+org-capture-central-project-notes-file "* %U %?\n %i\n %a" :heading "Notes" :prepend t)
+           ;; ("oc" "Project changelog" entry #'+org-capture-central-project-changelog-file "* %U %?\n %i\n %a" :heading "Changelog" :prepend t)
+           )
+         )
   (evil-define-key 'normal org-agenda-mode-map (kbd "q") #'org-agenda-quit)
   (evil-define-key 'normal org-agenda-mode-map (kbd "Q") #'org-agenda-Quit)
   )
